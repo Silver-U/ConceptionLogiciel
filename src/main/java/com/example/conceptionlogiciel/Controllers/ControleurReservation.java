010ce01db3020805035ec0c0634deb02c257e0f1
harlez16 <harlezloukelo@gmail.com>
2022-05-06 00:53:33 -0400

    Model, repository and Controleur de la classe Réservation.

---
+++ src/main/java/com/example/conceptionlogiciel/Controllers/ControleurReservation.java
@@ -0,0 +1,75 @@
+package com.example.conceptionlogiciel.Controllers;
+
+import com.example.conceptionlogiciel.Models.Reservation;
+import com.example.conceptionlogiciel.Models.Voiture.Vehicule;
+import com.example.conceptionlogiciel.Repository.ReservationRepository;
+import com.example.conceptionlogiciel.Repository.VehiculeRepository;
+
+import java.util.List;
+
+public class ControleurReservation {
+
+
+    private Reservation reservation;
+    private List<Reservation> reservations;
+    private ReservationRepository repo;
+
+    public ControleurReservation()
+    {
+        repo = new ReservationRepository();
+    }
+
+    public void createReservation()
+    {
+
+    }
+
+    public void insert(Reservation reservation)
+    {
+        repo.insert(reservation);
+    }
+
+    public boolean supprimerReservation(Reservation reservation)
+    {
+        if (reservationSupprimable())
+        {
+            repo.delete(reservation);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean reservationSupprimable()
+    {
+        return reservation.isEnLocationReservation();
+    }
+
+    public void getReservation(String numeroImmatriculation)
+    {
+        repo.getReservation(numeroImmatriculation);
+    }
+
+    public Reservation getReservation() {return reservation;}
+
+    public void setReservation(Reservation reservation) {this.reservation = reservation;}
+    public List<Reservation> getReservations()
+    {
+        return reservation;
+    }
+
+
+    public void setReservations(List<Reservation> reservations)
+    {
+        this.reservations = reservations;
+    }
+
+    public ReservationRepository getRepo()
+    {
+        return repo;
+    }
+
+    public void setRepo(ReservationRepository repo)
+    {
+        this.repo = repo;
+    }
+}
---
+++ src/main/java/com/example/conceptionlogiciel/Models/Reservation.java
@@ -0,0 +1,42 @@
+package com.example.conceptionlogiciel.Models;
+
+import com.example.conceptionlogiciel.Models.Voiture.Vehicule;
+
+public class Reservation {
+    private Client client;
+    private Vehicule vehicule;
+    private String idResevation;
+    private String dateFinResevation;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Vehicule getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(Vehicule vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public String getIdResevation() {
+        return idResevation;
+    }
+
+    public void setIdResevation(String idResevation) {
+        this.idResevation = idResevation;
+    }
+
+    public String getDateFinResevation() {
+        return dateFinResevation;
+    }
+
+    public void setDateFinResevation(String dateFinResevation) {
+        this.dateFinResevation = dateFinResevation;
+    }
+}
